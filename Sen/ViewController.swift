c75da64074ab16520391e9d042d0acd2dd1a9265
Odaryna <odaryna.korolevych@gmail.com>
2017-11-12 01:13:24 +0200

    Initial Commit

---
+++ Sen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sen
+//
+//  Created by Odaryna on 11/12/17.
+//  Copyright © 2017 Odaryna. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
